c038f2717c44da18a4dbcd05da7dbc3858429ff3
sushantgiri34 <sushantgoswami34@emailcom>
2026-01-17 16:02:21 +0530

    Improve logic and add loop for repeated calculations

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-##Input We Need From The User 
+## Input We Need From The User 
 #Total Rent 
 #Total food Ordered For Snacking 
 #Electricity Units spend 
 #charges per unit
 #persons living in room / flat 
 
-##output 
+## output 
 # total ammount you've to pay is 
 while True: 
     print("------------your rent calaculator-------------")
@@ -13,7 +13,7 @@ while True:
     rent = int(input("enter your hostel / flat / room rent $ : "))
     food = int(input("enter the ammount of food your ordered $ : "))
     electricity_spend = int(input("enter the total electricity you spend $ : "))
-    chargers_per_unit = float(input("enter the charges per unit (in floating value) : "))
+    chargers_per_unit = float(input("enter the charges per unit (in decimal value) / unit : "))
     persons = int(input("enter the number of persons living in hostel / flat / room (numbers of members) : "))
 
     total_bill = electricity_spend * chargers_per_unit
@@ -21,9 +21,10 @@ while True:
     total_ammount =  (rent + food + total_bill)
 
     output = total_ammount // persons
-
+    print("---------------------------------------------------")
     print(f"----- your total bill : {total_ammount} $-------")
     print(f"------each person wil pay : {output}$---------")
+    print("---------------------------------------------------")
 
     with open("rent_data.text","a") as file:
         file.write("------new calculator------\n")
@@ -36,4 +37,5 @@ while True:
     
     choice =  input("Do you want to calculate again? (yes/no): ").lower()
     if choice.lower() != 'yes':
+        print("!!!!!!!thank you for using rent calculator!!!!!!!")
         break 
